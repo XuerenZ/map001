e35f3d0b0be7e2d8d847459b713af3e90c97ca55
xuerenZ <328272127@qq.com>
2017-05-19 05:57:31 +0800

    创建工程

---
+++ map001/map001/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  map001
+//
+//  Created by acer on 17/5/19.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ map001/map001/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  map001
+//
+//  Created by acer on 17/5/19.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

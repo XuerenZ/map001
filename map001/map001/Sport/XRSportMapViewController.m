0cd06426c2467d6ca44a5bef96a17159d3062d71
xuerenZ <328272127@qq.com>
2017-05-24 06:16:05 +0800

    地图代理方法实现

--- map001/map001/Sport/XRSportMapViewController.m
+++ map001/map001/Sport/XRSportMapViewController.m
@@ -14,6 +14,10 @@
 @end
 
 @implementation XRSportMapViewController
+{
+    /// 起始位置
+    CLLocation *_startLocation;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -55,6 +59,86 @@
 }
 
 #pragma mark -MAMapViewDelegate
+/**
+ * @brief 位置或者设备方向更新后，会调用此函数
+ * @param mapView 地图View
+ * @param userLocation 用户定位信息(包括位置与设备方向等数据)
+ * @param updatingLocation 标示是否是location数据更新, YES:location数据更新 NO:heading数据更新
+ */
+- (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation
+{
+    // 0. 判断 `位置数据` 是否变化 - 不一定是经纬度变化！
+    if (!updatingLocation) {
+        return;
+    }
+    
+    // 大概 1s 更新一次！
+    NSLog(@"%@ %p", userLocation.location, userLocation.location);
+    
+    // 判断起始位置是否存在
+    if (_startLocation == nil) {
+        _startLocation = userLocation.location;
+        
+        // 1. 实例化大头针
+        MAPointAnnotation *annotaion = [MAPointAnnotation new];
+        
+        // 2. 指定坐标位置
+        annotaion.coordinate = userLocation.location.coordinate;
+        
+        // 3. 添加到地图视图
+        [mapView addAnnotation:annotaion];
+    }
+}
+
+/**
+ * @brief 单击地图回调，返回经纬度
+ * @param mapView 地图View
+ * @param coordinate 经纬度
+ */
+- (void)mapView:(MAMapView *)mapView didSingleTappedAtCoordinate:(CLLocationCoordinate2D)coordinate
+{
+    NSLog(@"%f %f", coordinate.latitude, coordinate.longitude);
+    
+    // 开始画线
+    // 1. 建立结构体的数组
+    CLLocationCoordinate2D coords[2];
+    
+    coords[0] = coordinate;
+    coords[1] = _startLocation.coordinate;
+    
+    // 2. 构造折线对象
+    MAPolyline *polyline = [MAPolyline polylineWithCoordinates:coords count:2];
+    
+    // 3. 添加到地图
+    [mapView addOverlay:polyline];
+}
+/**
+ * @brief 根据overlay生成对应的Renderer
+ * @param mapView 地图View
+ * @param overlay 指定的overlay
+ * @return 生成的覆盖物Renderer
+ */
+- (MAOverlayRenderer *)mapView:(MAMapView *)mapView rendererForOverlay:(id <MAOverlay>)overlay
+{
+    // 1. 判断 overlay 的类型
+    if (![overlay isKindOfClass:[MAPolyline class]]) {
+        return nil;
+    }
+    
+    // 2. 实例化折线渲染器
+    MAPolylineRenderer *renderer = [[MAPolylineRenderer alloc] initWithPolyline:overlay];
+    
+    // 3. 设置显示属性
+    renderer.lineWidth = 5;
+    renderer.strokeColor = [UIColor greenColor];
+    
+    return renderer;
+}
+/**
+ * @brief 当选中一个annotation views时，调用此接口
+ * @param mapView 地图View
+ * @param views 选中的annotation views
+ */
 - (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation
 {
     if ([annotation isKindOfClass:[MAPointAnnotation class]])

229240e0f34061855e5b2b8a2449e8e6802fbe76
xuerenZ <328272127@qq.com>
2017-07-07 07:32:18 +0800

    集成sharedSDK

--- map001/map001/Sport/XRSportCameraViewController.m
+++ map001/map001/Sport/XRSportCameraViewController.m
@@ -8,6 +8,8 @@
 
 #import "XRSportCameraViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKUI/ShareSDK+SSUI.h>
 @interface XRSportCameraViewController ()
 /**
  预览视图
@@ -38,6 +40,8 @@
     AVCaptureStillImageOutput *_imageOutput;
     /// 取景框 - 预览图层
     AVCaptureVideoPreviewLayer *_previewLayer;
+    /// 拍摄完成的图像
+    UIImage *_capturedPicture;
 }
 
 - (void)viewDidLoad {
@@ -47,6 +51,12 @@
     [self setupCaptureSession];
 }
 #pragma mark - 监听方法
+/**
+ 关闭窗口
+ */
+- (IBAction)close {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 /**
  拍照
  */
@@ -83,65 +93,94 @@
     }];
     
 }
-
 /**
- 拍照和保存
+ 切换摄像头
  */
-- (void)capturePicture {
+- (IBAction)switchCamera {
     
-    // AVCaptureConnection 表示图像和摄像头的连接
-    AVCaptureConnection *conn = _imageOutput.connections.firstObject;
-    
-    if (conn == nil) {
-        NSLog(@"无法连接到摄像头");
+    // 判断是否`正在取景` -> 需要做分享的工作
+    if (!_captureSession.isRunning) {
+        [self sharedPicture];
         
         return;
     }
     
-    // 拍摄照片
-    [_imageOutput captureStillImageAsynchronouslyFromConnection:conn completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
-        
-        // 判断是否有图像数据的采样缓冲区
-        if (imageDataSampleBuffer == nil) {
-            NSLog(@"没有图像数据采样缓冲区");
-            
-            return;
-        }
-        
-        // 使用图像数据采样缓冲区生成照片的数据
-        NSData *data = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
-        
-        // 使用数据生成图像 - 几乎可以显示在一个完整的视图中
-        UIImage *image = [UIImage imageWithData:data];
-        
-        // 将图像的上下两部分不显示在预览图层中的内容裁切掉！
-        // 1> 预览视图的大小
-        CGRect rect = self.previewView.bounds;
-        
-        // 2> 计算裁切掉的大小
-        CGFloat offset = (self.view.bounds.size.height - rect.size.height) * 0.5;
-        
-        // 3> 利用图像上下文来裁切图像
-        UIGraphicsBeginImageContextWithOptions(rect.size, YES, 0);
-        
-        // 4> 绘制图像
-        [image drawInRect:CGRectInset(rect, 0, -offset)];
-        
-        // 水印图像
-        [self.waterprintImageView.image drawInRect:self.waterprintImageView.frame];
-        
-        // 距离标签
-        [self.distanceLabel.attributedText drawInRect:self.distanceLabel.frame];
-        
-        // 5> 从图像上下文获取绘制结果
-        UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
-        
-        // 6> 关闭上下文
-        UIGraphicsEndImageContext();
-        
-        // 保存图像
-        UIImageWriteToSavedPhotosAlbum(result, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
-    }];
+    // 0. 具体的设备 - 摄像头／麦克风(模拟器没有摄像头，应该使用真机测试)
+    AVCaptureDevice *device = [self captureDevice];
+    
+    // 1. 创建输入设备
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:NULL];
+    
+    // 停止拍摄会话
+    [self stopCapture];
+    
+    // 删除之前的输入设备
+    [_captureSession removeInput:_inputDevice];
+    
+    // 2. 判断设备能否被添加 - 如果已经有摄像头在会话中，不能添加新的设备
+    if ([_captureSession canAddInput:input]) {
+        _inputDevice = input;
+    }
+    
+    // 3. 添加到会话
+    [_captureSession addInput:_inputDevice];
+    
+    // 4. 重新启动会话
+    [self startCapture];
+}
+
+/**
+ 分享照片
+ */
+- (void)sharedPicture {
+    
+    // 0. 判断是否有拍摄的图像
+    if (_capturedPicture == nil) {
+        return;
+    }
+    
+    // 1、创建分享参数 - 要分享图像的数组
+    NSArray *imageArray = @[_capturedPicture];
+    
+    NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+    [shareParams SSDKSetupShareParamsByText:@"分享内容"
+                                     images:imageArray
+                                        url:nil
+                                      title:@"分享标题"
+                                       type:SSDKContentTypeAuto];
+    
+    // 2. 分享（可以弹出我们的分享菜单和编辑界面）
+    [ShareSDK showShareActionSheet:nil
+                             items:nil
+                       shareParams:shareParams
+               onShareStateChanged:^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
+                   
+                   switch (state) {
+                       case SSDKResponseStateSuccess:
+                       {
+                           UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
+                                                                               message:nil
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"确定"
+                                                                     otherButtonTitles:nil];
+                           [alertView show];
+                           break;
+                       }
+                       case SSDKResponseStateFail:
+                       {
+                           UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                           message:[NSString stringWithFormat:@"%@",error]
+                                                                          delegate:nil
+                                                                 cancelButtonTitle:@"OK"
+                                                                 otherButtonTitles:nil, nil];
+                           [alert show];
+                           break;
+                       }
+                       default:
+                           break;
+                   }
+               }
+     ];
 }
 
 /**
@@ -210,6 +249,65 @@
     [self startCapture];
 }
 #pragma mark - 相机相关方法
+/**
+ 拍照和保存
+ */
+- (void)capturePicture {
+    
+    // AVCaptureConnection 表示图像和摄像头的连接
+    AVCaptureConnection *conn = _imageOutput.connections.firstObject;
+    
+    if (conn == nil) {
+        NSLog(@"无法连接到摄像头");
+        
+        return;
+    }
+    
+    // 拍摄照片
+    [_imageOutput captureStillImageAsynchronouslyFromConnection:conn completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        
+        // 判断是否有图像数据的采样缓冲区
+        if (imageDataSampleBuffer == nil) {
+            NSLog(@"没有图像数据采样缓冲区");
+            
+            return;
+        }
+        
+        // 使用图像数据采样缓冲区生成照片的数据
+        NSData *data = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+        
+        // 使用数据生成图像 - 几乎可以显示在一个完整的视图中
+        UIImage *image = [UIImage imageWithData:data];
+        
+        // 将图像的上下两部分不显示在预览图层中的内容裁切掉！
+        // 1> 预览视图的大小
+        CGRect rect = self.previewView.bounds;
+        
+        // 2> 计算裁切掉的大小
+        CGFloat offset = (self.view.bounds.size.height - rect.size.height) * 0.5;
+        
+        // 3> 利用图像上下文来裁切图像
+        UIGraphicsBeginImageContextWithOptions(rect.size, YES, 0);
+        
+        // 4> 绘制图像
+        [image drawInRect:CGRectInset(rect, 0, -offset)];
+        
+        // 水印图像
+        [self.waterprintImageView.image drawInRect:self.waterprintImageView.frame];
+        
+        // 距离标签
+        [self.distanceLabel.attributedText drawInRect:self.distanceLabel.frame];
+        
+        // 5> 从图像上下文获取绘制结果
+        UIImage *result = UIGraphicsGetImageFromCurrentImageContext();
+        
+        // 6> 关闭上下文
+        UIGraphicsEndImageContext();
+        
+        // 保存图像
+        UIImageWriteToSavedPhotosAlbum(result, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+    }];
+}
 
 /**
  开始拍摄
